9860dc15366d6d86303981b49322ae1657f6348a
highnoon77 <96806304+highnoon77@users.noreply.github.com>
2022-03-16 18:31:34 +0900

    Update CarDAO.java

--- SPRING13_RENTCAR/src/main/java/org/vision/rentcar/dao/CarDAO.java
+++ SPRING13_RENTCAR/src/main/java/org/vision/rentcar/dao/CarDAO.java
@@ -3,11 +3,12 @@ package org.vision.rentcar.dao;
 import java.util.List;
 
 import org.vision.rentcar.model.RentCar;
+import org.vision.rentcar.model.SearchCriteria;
 
 public interface CarDAO {
 	public void insert(RentCar dto);
 	public RentCar selectByNo(int no);
-	public List<RentCar> list();
+	public List<RentCar> list(SearchCriteria scri) throws Exception;
 	public void modify(RentCar dto);
 	public void delete(int no);
 }
